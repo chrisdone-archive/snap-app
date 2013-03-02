aa8caf2835a9b1cdc3e64e8b4dd2dfee2e13a937
Chris Done <chrisdone@gmail.com>
2013-03-02 11:19:52 +0100

    First commit.

---
+++ src/Snap/App/Controller.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE BangPatterns #-}
+{-# OPTIONS -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Controller routing/handling.
+
+module Snap.App.Controller
+  (runHandler
+  ,output
+  ,outputText
+  ,goHome
+  ,justOrGoHome
+  ,getInteger
+  ,getString
+  ,getStringMaybe
+  ,getPagination
+  ,getMyURI)
+  where
+
+import Snap.Core
+import Snap.App.Types
+
+import Control.Applicative
+import Control.Monad.Env
+import Control.Monad.Reader       (runReaderT)
+import Data.ByteString            (ByteString)
+import Data.ByteString.UTF8       (toString)
+import Data.Maybe
+import Network.URI
+import Data.Text.Lazy             (Text,toStrict)
+import Database.PostgreSQL.Base   (withPoolConnection)
+import Database.PostgreSQL.Simple (Pool)
+import Safe                       (readMay)
+import Text.Blaze                 (Html)
+import Text.Blaze.Renderer.Text   (renderHtml)
+
+-- | Run a controller handler.
+runHandler :: s -> c -> Pool -> Controller c s () -> Snap ()
+runHandler st conf pool ctrl = do
+  withPoolConnection pool $ \conn -> do
+    let state = ControllerState conf conn st
+    -- Default to HTML, can be overridden.
+    modifyResponse $ setContentType "text/html"
+    runReaderT (runController ctrl) state
+
+-- | Strictly renders HTML to Text before outputting it via Snap.
+--   This ensures that any lazy exceptions are caught by the Snap
+--   handler.
+output :: Html -> Controller c s ()
+output html = outputText $ renderHtml $ html
+
+-- | Strictly renders text before outputting it via Snap.
+--   This ensures that any lazy exceptions are caught by the Snap
+--   handler.
+outputText :: Text -> Controller c s ()
+outputText text = do
+  let !x = toStrict $ text
+  writeText x
+
+-- | Generic redirect to home page.
+goHome :: Controller c s ()
+goHome = redirect "/"
+
+-- | Extract a Just value or go home.
+justOrGoHome :: Maybe a -> (a -> Controller c s ()) -> Controller c s ()
+justOrGoHome x m = maybe goHome m x
+
+-- | Get integer parmater.
+getInteger :: ByteString -> Integer -> Controller c s Integer
+getInteger name def = do
+  pid <- (>>= readMay . toString) <$> getParam name
+  maybe (return def) return pid
+
+-- | Get string.
+getString :: ByteString -> String -> Controller c s String
+getString name def = do
+  pid <- (>>= return . toString) <$> getParam name
+  maybe (return def) return pid
+
+-- | Get string (maybe).
+getStringMaybe :: ByteString -> Controller c s (Maybe String)
+getStringMaybe name = do
+  pid <- (>>= return . toString) <$> getParam name
+  return pid
+
+-- | Get pagination data.
+getPagination :: AppConfig c => Controller c s Pagination
+getPagination = do
+  p <- getInteger "page" 1
+  limit <- getInteger "limit" 35
+  uri <- getMyURI
+  return Pagination { pnPage = max 1 p
+                    , pnLimit = max 1 (min 100 limit)
+                    , pnURI = uri
+                    , pnResults = 0
+                    , pnTotal = 0
+                    }
+
+getMyURI :: AppConfig c => Controller c s URI
+getMyURI = do
+  domain <- env (getConfigDomain . controllerStateConfig)
+  fmap (fromJust .
+	parseURI .
+	(("http://" ++ domain) ++) .
+	toString .
+	rqURI)
+       getRequest
---
+++ src/Snap/App/Model.hs
@@ -0,0 +1,63 @@
+{-# OPTIONS -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+-- | Model running.
+
+module Snap.App.Model
+  (model
+  ,query
+  ,single
+  ,singleNoParams
+  ,queryNoParams
+  ,exec
+  ,DB.Only(..))
+  where
+
+import           Control.Monad.Env                       (env)
+
+import           Control.Monad.Reader
+import           Data.String
+import           Database.PostgreSQL.Simple              (Only(..))
+import qualified Database.PostgreSQL.Simple              as DB
+import           Database.PostgreSQL.Simple.QueryParams
+import           Database.PostgreSQL.Simple.QueryResults
+import           Snap.App.Types
+
+-- | Run a model action.
+model :: AppLiftModel c s => Model c s a -> Controller c s a
+model = liftModel
+
+-- | Query with some parameters.
+query :: (QueryParams ps,QueryResults r) => [String] -> ps -> Model c s [r]
+query q ps = do
+  conn <- env modelStateConn
+  Model $ ReaderT (\_ -> DB.query conn (fromString (unlines q)) ps)
+
+-- | Query a single field from a single result.
+single :: (QueryParams ps,QueryResults (Only r)) => [String] -> ps -> Model c s (Maybe r)
+single q ps = do
+  rows <- query q ps
+  case rows of
+    [(Only r)] -> return (Just r)
+    _          -> return Nothing
+
+-- | Query a single field from a single result (no params).
+singleNoParams :: (QueryResults (Only r)) => [String] -> Model c s (Maybe r)
+singleNoParams q = do
+  rows <- queryNoParams q
+  case rows of
+    [(Only r)] -> return (Just r)
+    _          -> return Nothing
+
+-- | Query with no parameters.
+queryNoParams :: (QueryResults r) => [String] -> Model c s [r]
+queryNoParams q = do
+  conn <- env modelStateConn
+  Model $ ReaderT (\_ -> DB.query_ conn (fromString (unlines q)))
+
+-- | Execute some SQL returning the rows affected.
+exec :: (QueryParams ps) => [String] -> ps -> Model c s Integer
+exec q ps = do
+  conn <- env modelStateConn
+  Model $ ReaderT (\_ -> DB.execute conn (fromString (unlines q)) ps)
